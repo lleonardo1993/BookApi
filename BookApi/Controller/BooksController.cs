3188b3834bd081a151a37ebdaea087ed3bd177f4
Leonardo <lleonardo1555@gmail.com>
2023-01-17 21:57:56 -0300

    Finalização dos end points.

---
+++ BookApi/Controller/BooksController.cs
@@ -0,0 +1,74 @@
+﻿using BookApi.Model;
+using BookApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBooks(int id)
+        {
+            return await _bookRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBooks([FromBody] Book book)
+        {
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var bookToDelete = await _bookRepository.Get(id);
+
+            if (bookToDelete == null)
+
+                return NotFound();
+
+            await _bookRepository.Delete(bookToDelete.Id);
+
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutBooks(int id, [FromBody] Book book)
+        {
+            if (id == book.Id)
+            {
+                await _bookRepository.Update(book);
+
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+
+
+        }
+
+    }
+}
--- BookApi/Startup.cs
+++ BookApi/Startup.cs
@@ -1,4 +1,5 @@
 using BookApi.Model;
+using BookApi.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,7 +29,9 @@ namespace BookApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddDbContext<BookContext>(x => x.UseSqlite("Data source=books.db"));
+            services.AddScoped<IBookRepository, BookRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
